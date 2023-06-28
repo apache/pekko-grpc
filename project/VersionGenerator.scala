9719f0e5e7d42d735f7bf613555d8b41650c0670
PJ Fanning <pjfanning@users.noreply.github.com>
2023-06-28 01:38:55 +0100

    better to link to pekko source than akka source

--- project/VersionGenerator.scala
+++ project/VersionGenerator.scala
@@ -13,7 +13,7 @@ import sbt._
 /**
  * Generate version.conf file based on the version setting.
  *
- * This was adapted from https://github.com/akka/akka/blob/v2.6.8/project/VersionGenerator.scala
+ * This was adapted from https://github.com/apache/incubator-pekko/blob/main/project/VersionGenerator.scala
  */
 object VersionGenerator {
 
