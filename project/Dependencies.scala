21faf7e90f812aabfd3bf0a7412bb80d8ee0eeed
PJ Fanning <pjfanning@users.noreply.github.com>
2023-11-11 14:25:01 +0100

    io.grpc 1.59

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -32,12 +32,12 @@ object Dependencies {
     val pekkoHttp = "1.0.0"
     val pekkoHttpBinary = "current"
 
-    val grpc = "1.54.2" // checked synced by VersionSyncCheckPlugin
+    val grpc = "1.59.0" // checked synced by VersionSyncCheckPlugin
     // Even referenced explicitly in the sbt-plugin's sbt-tests
     // If changing this, remember to update protoc plugin version to align in
     // maven-plugin/src/main/maven/plugin.xml and org.apache.pekko.grpc.sbt.PekkoGrpcPlugin
     val googleProtoc = "3.20.1" // checked synced by VersionSyncCheckPlugin
-    val googleProtobufJava = "3.21.12"
+    val googleProtobufJava = "3.24.0"
 
     val scalaTest = "3.2.15"
 
