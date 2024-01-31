0e192ef1e33886609ab02f69ea81db41f9db208c
Arnout Engelen <arnout@bzzt.net>
2024-01-31 01:04:14 +0100

    Update Scala 3 to 3.3.2
    Follow-up on #195
    
    Should get us the fix for https://github.com/lampepfl/dotty/issues/17330

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
     // Update the .github workflows when these scala versions change
     val scala212 = "2.12.18"
     val scala213 = "2.13.12"
-    val scala3 = "3.3.1"
+    val scala3 = "3.3.2"
 
     // the order in the list is important because the head will be considered the default.
     val CrossScalaForLib = Seq(scala212, scala213, scala3)
