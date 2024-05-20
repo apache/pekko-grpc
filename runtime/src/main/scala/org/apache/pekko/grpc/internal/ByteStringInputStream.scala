73b258ee02de11e15a3cdcdc35eb33dffbcb59a1
PJ Fanning <pjfanning@users.noreply.github.com>
2024-05-20 19:08:19 +0100

    simpler code
    Co-Authored-By: João Ferreira <943051+jtjeferreira@users.noreply.github.com>

--- runtime/src/main/scala/org/apache/pekko/grpc/internal/ByteStringInputStream.scala
+++ runtime/src/main/scala/org/apache/pekko/grpc/internal/ByteStringInputStream.scala
@@ -14,14 +14,10 @@
 package org.apache.pekko.grpc.internal
 
 import java.io.{ ByteArrayInputStream, InputStream, SequenceInputStream }
-import java.lang.invoke.{ MethodHandles, MethodType }
-
-import scala.util.Try
 
 import org.apache.pekko
 import pekko.annotation.InternalApi
 import pekko.util.ByteString
-import pekko.util.ByteString.ByteString1C
 import pekko.util.ByteString.ByteStrings
 import pekko.util.ccompat.JavaConverters._
 
@@ -29,31 +25,10 @@ import pekko.util.ccompat.JavaConverters._
 @InternalApi
 private[internal] object ByteStringInputStream {
 
-  private val byteStringInputStreamMethodTypeOpt = Try {
-    val lookup = MethodHandles.publicLookup()
-    val inputStreamMethodType = MethodType.methodType(classOf[InputStream])
-    lookup.findVirtual(classOf[ByteString], "asInputStream", inputStreamMethodType)
-  }.toOption
-
   def apply(bs: ByteString): InputStream = bs match {
-    case cs: ByteString1C =>
-      getInputStreamUnsafe(cs)
-    case _ =>
-      if (byteStringInputStreamMethodTypeOpt.isDefined) {
-        byteStringInputStreamMethodTypeOpt.get.invoke(bs).asInstanceOf[InputStream]
-      } else {
-        legacyConvert(bs)
-      }
-  }
-
-  private def legacyConvert(bs: ByteString): InputStream = bs match {
     case bss: ByteStrings =>
-      new SequenceInputStream(bss.bytestrings.iterator.map(legacyConvert).asJavaEnumeration)
+      new SequenceInputStream(bss.bytestrings.iterator.map(apply).asJavaEnumeration)
     case _ =>
-      getInputStreamUnsafe(bs)
+      new ByteArrayInputStream(bs.toArrayUnsafe())
   }
-
-  private def getInputStreamUnsafe(bs: ByteString): InputStream =
-    new ByteArrayInputStream(bs.toArrayUnsafe())
-
 }
