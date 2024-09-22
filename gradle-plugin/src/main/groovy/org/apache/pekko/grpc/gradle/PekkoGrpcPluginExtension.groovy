be56dae982a074c7ecc3f48e84c58257c837dca1
PJ Fanning <pjfanning@users.noreply.github.com>
2024-09-22 01:48:00 +0100

    protobuf 4.28.2

--- gradle-plugin/src/main/groovy/org/apache/pekko/grpc/gradle/PekkoGrpcPluginExtension.groovy
+++ gradle-plugin/src/main/groovy/org/apache/pekko/grpc/gradle/PekkoGrpcPluginExtension.groovy
@@ -14,7 +14,7 @@ import org.gradle.api.Project
 
 class PekkoGrpcPluginExtension {
 
-    static final String PROTOC_VERSION = "3.25.6" // checked synced by VersionSyncCheckPlugin
+    static final String PROTOC_VERSION = "4.30.0" // checked synced by VersionSyncCheckPlugin
 
     static final String PROTOC_PLUGIN_SCALA_VERSION = "2.12"
 
