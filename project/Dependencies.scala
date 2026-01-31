cccf37151d28e171e7caef95700a9f242225e743
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-01-31 19:49:20 +0100

    Update logback-classic to 1.5.27 (#616)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -77,7 +77,7 @@ object Dependencies {
   }
 
   object Runtime {
-    val logback = "ch.qos.logback" % "logback-classic" % "1.5.26" % "runtime"
+    val logback = "ch.qos.logback" % "logback-classic" % "1.5.27" % "runtime"
   }
 
   object Protobuf {
