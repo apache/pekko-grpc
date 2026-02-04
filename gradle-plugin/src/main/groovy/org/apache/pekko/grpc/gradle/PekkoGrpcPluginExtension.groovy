6b9a75df8b4a9c717f865fd0630c1eeb64d8a885
PJ Fanning <pjfanning@users.noreply.github.com>
2026-02-04 18:54:25 +0100

    more places

--- gradle-plugin/src/main/groovy/org/apache/pekko/grpc/gradle/PekkoGrpcPluginExtension.groovy
+++ gradle-plugin/src/main/groovy/org/apache/pekko/grpc/gradle/PekkoGrpcPluginExtension.groovy
@@ -18,7 +18,7 @@ class PekkoGrpcPluginExtension {
 
     static final String PROTOC_PLUGIN_SCALA_VERSION = "2.12"
 
-    static final String GRPC_VERSION = "1.78.0" // checked synced by VersionSyncCheckPlugin
+    static final String GRPC_VERSION = "1.79.0" // checked synced by VersionSyncCheckPlugin
 
     static final String PLUGIN_CODE = 'org.apache.pekko.grpc.gradle'
 
