c5bdd708360bf00f25d7050fe932ec191a48105f
虎鸣 <hepin.p@alibaba-inc.com>
2026-04-27 14:09:38 +0800

    perf: reduce gRPC protobuf frame allocations

--- runtime/src/main/scala/org/apache/pekko/grpc/javadsl/GoogleProtobufSerializer.scala
+++ runtime/src/main/scala/org/apache/pekko/grpc/javadsl/GoogleProtobufSerializer.scala
@@ -15,17 +15,30 @@ package org.apache.pekko.grpc.javadsl
 
 import org.apache.pekko
 import pekko.annotation.ApiMayChange
-import pekko.grpc.ProtobufSerializer
+import pekko.grpc.ProtobufFrameSerializer
+import pekko.grpc.internal.AbstractGrpcProtocol
 import pekko.util.ByteString
+import com.google.protobuf.CodedOutputStream
 import com.google.protobuf.Parser
 
 import java.io.InputStream
 
 @ApiMayChange
-class GoogleProtobufSerializer[T <: com.google.protobuf.Message](parser: Parser[T]) extends ProtobufSerializer[T] {
+class GoogleProtobufSerializer[T <: com.google.protobuf.Message](parser: Parser[T]) extends ProtobufFrameSerializer[T] {
 
   override def serialize(t: T): ByteString =
     ByteString.fromArrayUnsafe(t.toByteArray)
+  override private[grpc] def serializeDataFrame(t: T): ByteString = {
+    val dataLength = t.getSerializedSize
+    val frame = new Array[Byte](AbstractGrpcProtocol.FrameHeaderSize + dataLength)
+    AbstractGrpcProtocol.writeFrameHeader(frame, 0, dataLength, isCompressed = false, isTrailer = false)
+
+    val output = CodedOutputStream.newInstance(frame, AbstractGrpcProtocol.FrameHeaderSize, dataLength)
+    t.writeTo(output)
+    output.checkNoSpaceLeft()
+
+    ByteString.fromArrayUnsafe(frame)
+  }
   override def deserialize(bytes: ByteString): T = {
     val inputStream = bytes.asInputStream
     try parser.parseFrom(inputStream)
