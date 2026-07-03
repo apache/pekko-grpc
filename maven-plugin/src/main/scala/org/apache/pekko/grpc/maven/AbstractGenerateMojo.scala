641d969ecbd468e223749f9645bd81c5c65bfe82
He-Pin(kerr) <hepin1989@gmail.com>
2026-07-03 11:13:00 +0800

    Remove private[this] from Maven plugin (#771)
    Motivation:
    Avoid using private[this] in Scala sources.
    
    Modification:
    Replace the remaining Maven plugin private[this] method modifiers with ordinary private modifiers.
    
    Result:
    The repository no longer contains private[this] usages.
    
    Tests:
    - rg -n "private\[this\]" . / no matches
    - scalafmt --mode diff-ref=origin/main / passed
    - scalafmt --list --mode diff-ref=origin/main / passed
    - sbt "maven-plugin / Compile / compile" / passed
    - git diff --check / passed
    
    References:
    None - cleanup requested separately from #766

--- maven-plugin/src/main/scala/org/apache/pekko/grpc/maven/AbstractGenerateMojo.scala
+++ maven-plugin/src/main/scala/org/apache/pekko/grpc/maven/AbstractGenerateMojo.scala
@@ -221,7 +221,7 @@ abstract class AbstractGenerateMojo @Inject() (buildContext: BuildContext) exten
     }
   }
 
-  private[this] def executeProtoc(
+  private def executeProtoc(
       protocCommand: Seq[String] => Int,
       schemas: Set[File],
       protocOptions: Seq[String],
@@ -242,7 +242,7 @@ abstract class AbstractGenerateMojo @Inject() (buildContext: BuildContext) exten
         throw new RuntimeException("error occurred while compiling protobuf files: %s".format(e.getMessage), e)
     }
 
-  private[this] def compile(
+  private def compile(
       protocCommand: Seq[String] => Int,
       schemas: Set[File],
       protoDir: File,
