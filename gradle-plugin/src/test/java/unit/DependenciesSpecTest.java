44a8b3443327c58eb3a48a80a75a9063f02e940e
PJ Fanning <pjfanning@users.noreply.github.com>
2026-07-24 16:39:49 +0100

    more

--- gradle-plugin/src/test/java/unit/DependenciesSpecTest.java
+++ gradle-plugin/src/test/java/unit/DependenciesSpecTest.java
@@ -32,7 +32,7 @@ class DependenciesSpecTest extends BaseSpec {
     static final String PROTOC_PLUGIN_CODEGEN = "pekkoGrpc";
     static final String PROTOC_PLUGIN_SCALAPB = "scalapb";
     static final String PROTOC_PLUGIN_SCALA_VERSION = "2.12";
-    static final String GRPC_VERSION = "1.82.2";
+    static final String GRPC_VERSION = "1.83.0";
 
     private Project project;
 
