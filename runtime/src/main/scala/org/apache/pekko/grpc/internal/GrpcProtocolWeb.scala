6a402dc9aeba4b1cd1669e60217ef752acda75ec
PJ Fanning <pjfanning@users.noreply.github.com>
2026-07-31 14:01:03 +0100

    Update GrpcProtocolWeb.scala

--- runtime/src/main/scala/org/apache/pekko/grpc/internal/GrpcProtocolWeb.scala
+++ runtime/src/main/scala/org/apache/pekko/grpc/internal/GrpcProtocolWeb.scala
@@ -82,15 +82,35 @@ abstract class GrpcProtocolWebBase(subType: String) extends AbstractGrpcProtocol
   }
 
   private final def decodeTrailer(data: ByteString): List[HttpHeader] = {
-    data.utf8String.split("\n").iterator.filter(_.nonEmpty).flatMap { line =>
-      val trimmed = line.stripSuffix("\r")
-      val colon = trimmed.indexOf(':')
-      if (colon > 0) {
-        val key = trimmed.substring(0, colon).trim
-        val value = trimmed.substring(colon + 1).trim
-        if (key.nonEmpty) Some(RawHeader(key, value)) else None
-      } else None
-    }.toList
+    val str = data.utf8String
+    val len = str.length
+    val headers = List.newBuilder[HttpHeader]
+    var i = 0
+    while (i < len) {
+      // skip leading whitespace and blank lines
+      while (i < len &&
+        (str.charAt(i) == ' ' || str.charAt(i) == '\t' || str.charAt(i) == '\r' || str.charAt(i) == '\n'))
+        i += 1
+      if (i >= len) return headers.result()
+      // scan for colon (key:value separator)
+      val keyStart = i
+      while (i < len && str.charAt(i) != ':') i += 1
+      if (i >= len) return headers.result() // no colon found, malformed
+      val keyEnd = i
+      i += 1 // skip ':'
+      // scan for LF (line terminator)
+      val valueStart = i
+      while (i < len && str.charAt(i) != '\n') i += 1
+      var valueEnd = i
+      // strip trailing CR
+      if (valueEnd > valueStart && str.charAt(valueEnd - 1) == '\r') valueEnd -= 1
+      i += 1 // skip LF
+      // trim and emit
+      val key = str.substring(keyStart, keyEnd).trim
+      val value = str.substring(valueStart, valueEnd).trim
+      if (key.nonEmpty) headers += RawHeader(key, value)
+    }
+    headers.result()
   }
 
 }
