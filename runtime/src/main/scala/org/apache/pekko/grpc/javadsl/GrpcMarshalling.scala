9f9392d51ca68031bd72bb674fae1ca05cdcf0f2
Tim Whittington <tim.whittington@indexity.io>
2026-08-18 20:35:49 +1200

    Simplify GrpcMarshalling.handleUnaryResponse in Java DSL.
    CompleteableFuture already applies FastFuture like immediate dispatch on completed futures with equivalent performance.

--- runtime/src/main/scala/org/apache/pekko/grpc/javadsl/GrpcMarshalling.scala
+++ runtime/src/main/scala/org/apache/pekko/grpc/javadsl/GrpcMarshalling.scala
@@ -133,26 +133,9 @@ object GrpcMarshalling {
       writer: GrpcProtocolWriter,
       system: ClassicActorSystemProvider,
       eHandler: JFunction[ActorSystem, JFunction[Throwable, Trailers]]): CompletionStage[HttpResponse] =
-    try {
-      response match {
-        case future: CompletableFuture[?] if future.isDone =>
-          try completedResponse(marshal(completedValue[Out](future), m, writer, system, eHandler))
-          catch {
-            case NonFatal(error) => handleUnaryFailure(error, writer, system, eHandler)
-          }
-        case _ =>
-          response
-            .thenApply(out => marshal(out, m, writer, system, eHandler))
-            .exceptionally(error => GrpcExceptionHandler.standard(error, eHandler, writer, system))
-      }
-    } catch {
-      case NonFatal(error) => handleUnaryFailure(error, writer, system, eHandler)
-    }
-
-  @InternalApi
-  def handleUnaryFailure(error: Throwable): CompletionStage[HttpResponse] =
-    if (NonFatal(error)) failure(error)
-    else throw error
+    response
+      .thenApply(out => marshal(out, m, writer, system, eHandler))
+      .exceptionally(error => GrpcExceptionHandler.standard(error, eHandler, writer, system))
 
   @InternalApi
   def handleUnaryFailure(
@@ -178,6 +161,4 @@ object GrpcMarshalling {
       case NonFatal(error) => failure(error)
     }
 
-  private def completedValue[T](future: CompletableFuture[?]): T =
-    future.asInstanceOf[CompletableFuture[T]].getNow(null.asInstanceOf[T])
 }
