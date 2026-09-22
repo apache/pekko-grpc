e85558a94db6ff493a04bc743d661e1bb89b3fd7
PJ Fanning <pjfanning@users.noreply.github.com>
2026-09-22 15:21:36 +0100

    more changes

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -45,7 +45,7 @@ object Dependencies {
     // Even referenced explicitly in the sbt-plugin's sbt-tests
     // If changing this, remember to update protoc plugin version to align in
     // maven-plugin/src/main/maven/plugin.xml and org.apache.pekko.grpc.sbt.PekkoGrpcPlugin
-    val googleProtoc = "4.36.1" // checked synced by VersionSyncCheckPlugin
+    val googleProtoc = "4.36.2" // checked synced by VersionSyncCheckPlugin
     val googleProtobufJava = "4.36.2"
 
     val scalaTest = "3.2.20"
