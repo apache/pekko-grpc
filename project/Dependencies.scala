a87837538522b8e66852075325c941dfd795847a
Scala Steward <scala_steward@virtuslab.com>
2026-09-22 13:13:31 +0000

    Update protobuf-java to 4.36.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -46,7 +46,7 @@ object Dependencies {
     // If changing this, remember to update protoc plugin version to align in
     // maven-plugin/src/main/maven/plugin.xml and org.apache.pekko.grpc.sbt.PekkoGrpcPlugin
     val googleProtoc = "4.36.1" // checked synced by VersionSyncCheckPlugin
-    val googleProtobufJava = "4.36.1"
+    val googleProtobufJava = "4.36.2"
 
     val scalaTest = "3.2.20"
 
