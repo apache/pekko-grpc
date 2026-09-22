fd44957f13006e772204d842da7a7f4c35a98bea
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-09-22 17:24:03 +0200

    Update protobuf-java to 4.36.2 (#893)
    * Update protobuf-java to 4.36.2
    
    * more changes
    
    ---------
    
    Co-authored-by: PJ Fanning <pjfanning@users.noreply.github.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -45,8 +45,8 @@ object Dependencies {
     // Even referenced explicitly in the sbt-plugin's sbt-tests
     // If changing this, remember to update protoc plugin version to align in
     // maven-plugin/src/main/maven/plugin.xml and org.apache.pekko.grpc.sbt.PekkoGrpcPlugin
-    val googleProtoc = "4.36.1" // checked synced by VersionSyncCheckPlugin
-    val googleProtobufJava = "4.36.1"
+    val googleProtoc = "4.36.2" // checked synced by VersionSyncCheckPlugin
+    val googleProtobufJava = "4.36.2"
 
     val scalaTest = "3.2.20"
 
